cb4c4b90d855bf19c740f4cb1b35168facb7f48d
Marco Consiglio <91251132+marcocreazioni@users.noreply.github.com>
2021-10-21 21:15:38 +0200

    Add files via upload

---
+++ modulo.php
@@ -0,0 +1,33 @@
+<?php
+if($_POST['nome'] != '' && $_POST['cognome'] != '' && $_POST['indirizzo'] != '' && $_POST['email'] != ''  && $_POST['prefisso'] != '' && $_POST['numero'] != ''           && $_POST['citta'] != '' && $_POST['stato'] != '' && $_POST['privacy'] != '' && $_POST['cap'] != '' && $_POST['subject'] != '' && $_POST['comment'] != '') {
+	
+	
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+	$headers .= 'From: ' . $_POST["nome"] . $_POST['cognome'] . $_POST['indirizzo'] . $_POST['citta']. $_POST['stato'] . '<' . $_POST["email"] .'>'. $_POST['privacy'] . $_POST['cap']  . "\r\n" .
+    'Reply-To: ' . $_POST["email"] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+	
+	$subject = 'Nuova mail dal sito ';
+	$message = '<b>Name:</b> '.$_POST["nome"].'<br>
+	<b>Email:</b> '.$_POST["email"].'<br>
+    <b>Prefisso:</b> '.$_POST["prefisso"] .'<b>Numero:</b>' .$_POST["numero"] .'<br>
+	<b>Subject:</b> '.$_POST["subject"].'<br>
+	<b>Message:</b> '.$_POST["comment"];
+	
+	mail( "info@indirizzosito", $subject, $message, $headers ); //  Replace with your email 
+	
+	echo '<div class="alert alert-success alert-dismissable">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+			Il tuo messaggio è stato inviato
+		</div>';
+	
+}else{
+			
+	echo '<div class="alert alert-danger alert-dismissable">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+			<b>Ci sono dei campi da compilare!</b>
+		</div>';
+}
+?>

488ceb21653a2fdcf5db4e8a4c4c2dc1ccaf560e
Marco Consiglio <91251132+marcocreazioni@users.noreply.github.com>
2025-10-21 13:30:08 +0200

    Add files via upload

--- file.js
+++ file.js
@@ -1,2 +1,11 @@
-/* qui usiamo il codice javascript */
-document.writeln('Ciao Mondo');
+//QUi posso usare codice javascript
+//stampa nel documento del valore di una variabile
+//DIchiarazione di una variabilie var + nome della
+//variabile
+var a;
+//assegnare un valore ad una variabile dichiarata
+a = "Benvenuto";
+//dichiarazione e assegnazione
+var b = "in questa lezione";
+var c = "di oggi";
+document.writeln(a + " " + b + " " + c);

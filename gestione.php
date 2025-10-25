83f195131d77c7529a9325c9210dad4c9cd602e3
Marco Consiglio <91251132+marcocreazioni@users.noreply.github.com>
2025-10-25 10:00:18 +0200

    Add files via upload

--- gestione.php
+++ gestione.php
@@ -1,14 +1,20 @@
 <?php
 //prelievo di variabili da form utente (usando una variabile super globale)
 $testo1 = $_POST['nome'];
-define("testo2",$_POST['cognome']);
+$testo2 = $_POST['cognome'];
+$testo3 = $_POST['sito'];
+//conversione delle tipologie di variabile 
+//nella parentesi tonda indichiamo la tipologia per la conversione seguito dalla variabile da convertire
+$testo3 = (int) $testo3;
 
-function insert($cognome){
-    $congnome = $_POST['cognome'];
-    echo $cognome + "Rossi"
-   
+//Prelievo e la conversione del campo serivzi
+$testo4 = $_POST['servizi'];
+$testo4 = (int) $testo4;
 
-}
+//Operazione da svolgere
+$totale = $testo3 + $testo4;
 
-echo $testo1 . " " . testo2 . insert();
+echo $testo1 . " " . $testo2 . "</br>" .
+ "<p>Il prezzo del sito web è:" . $testo3 ." €</p>". 
+ "<p>Il totale incluse le opzioni è:" . $totale ." €</p>";
 ?>
